0c89ceafa8f8f19fa17fdb3e507491ac61aa174c
wignn <tigfiragnafatur1933@gmail.com>
2024-11-14 21:32:58 +0700

    delll

--- src/main.rs
+++ src/main.rs
@@ -32,3 +32,26 @@ fn shadowing(){
     let apple = 10;
     println!("{}", apple);
 }
+
+
+#[test]
+fn scalar_data_types(){
+    let x = 10;
+    let y = 10.0;
+    let z = true;
+    let a = 'a';
+    println!("x: {}, y: {}, z: {}, a: {}", x, y, z, a);
+}
+
+
+#[test]
+fn oprations(){
+    let x = 10;
+    let y = 20;
+    let z = x + y;
+    let a = x - y;
+    let b = x * y;
+    let c = x / y;
+    let d = x % y;
+    println!("z: {}, a: {}, b: {}, c: {}, d: {}", z, a, b, c, d);
+}
